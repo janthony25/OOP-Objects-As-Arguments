7708baf7066674740b0e1066f6ec08b6e236deac
janthony25 <junanthony.business@gmail.com>
2024-02-04 10:14:08 +1300

    Add project files.

---
+++ OOP-Objects-As-Arguments/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car("Mustang", "red");
+            // use this
+            Car car2 = Copy(car1); 
+            // instead of this
+            // Car car2 = new Car(car.model, car.color)
+
+            // ChangeColor(car1, "blue");
+
+            Console.WriteLine($"{car2.color} {car2.model}");
+
+            Console.ReadKey();
+        }
+
+        // make new copy
+        public static Car Copy(Car car)
+        {
+            return new Car(car.model, car.color);
+        }
+
+
+        /*
+        // change object
+        public static void ChangeColor(Car cars, string color)
+        {
+            cars.color = color;
+        }
+
+        */
+    }
+
+    class Car
+    {
+        public string model;
+        public string color;
+        public Car(string model, string color)
+        {
+            this.model = model;
+            this.color = color;
+        }
+    }
+}
